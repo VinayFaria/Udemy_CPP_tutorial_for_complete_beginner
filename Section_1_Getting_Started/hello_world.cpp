037fcb73b7c92b9d4e04190746b7c4c8de3fc70c
Vinay Faria <46863395+VinayFaria@users.noreply.github.com>
2023-02-10 23:10:07 +0530

    Hello world file

---
+++ Section_1_Getting_Started/hello_world.cpp
@@ -0,0 +1,11 @@
+// Practicing c++ from the course "C++ Tutorial for Complete Beginners", this is the first file "Hello World"
+// Running from cmd: "g++ -o output_file_name  source.cpp" will create output_file_name.exe after that running this file we will get output
+// Display contents of c++ file: type source.cpp
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello World" << endl;
+    return 0;
+}
