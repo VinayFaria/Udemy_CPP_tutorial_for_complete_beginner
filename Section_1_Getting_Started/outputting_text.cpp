afbbbb5588289e7e007241662c22be474bec5fc0
VinayFaria <vinayfaria1998@gmail.com>
2023-02-11 18:20:18 +0530

    File containing more methods to get output

---
+++ Section_1_Getting_Started/outputting_text.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "first sentence..." << flush;
+    cout << "second sentence...";
+    cout << "third sentence...\n";
+    cout << "second line..." << endl;
+    cout << "Apple " << "Banana " << "Cherry" << endl;
+    cout << "last line" << endl;
+    return 0;
+}
