15c1d6887a7b33dd144e00667e31f22b05b543df
VinayFaria <vinayfaria1998@gmail.com>
2023-02-15 11:47:11 +0530

    example of boolean use cases

---
+++ Section_2_Basic_Syntax/boolean.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    bool bvalue = true; // The bool type can have values true and false.
+    cout << bvalue << endl;
+
+    bvalue = 1; // any number other than 0 is also considered as true
+    cout << bvalue << endl;
+
+    bvalue = 45;
+    cout << bvalue << endl;
+
+    bvalue = -45;
+    cout << bvalue << endl;
+
+    bvalue = false;
+    cout << bvalue << endl;
+
+    bvalue = 0;
+    cout << bvalue << endl;
+
+    return 0;
+}
