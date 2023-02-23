1092adb1232107174b8d1d0ec26f75cfd48f16e1
VinayFaria <vinayfaria1998@gmail.com>
2023-02-23 11:41:53 +0530

    importing and using a file saved in same project

---
+++ Section_3_Subroutines/headers_and_prototypes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "utils.h"
+
+using namespace std;
+
+int main() {
+    print_something();
+
+    return 0;
+}
+
+void print_something() {
+    cout << "Hello this was nice and important lesson i.e. to understand importing files and defining function in a different way" << endl;
+}
---
+++ Section_3_Subroutines/utils.h
@@ -0,0 +1,6 @@
+#ifndef UTILS_H_
+#define UTILS_H_
+
+void print_something();
+
+#endif // UTILS_H_
